5685e7723f04bea9c799f92942a7de3f01f01b07
Iago Oliveira <iagor2d2@gmail.com>
2026-09-23 15:16:20 -0300

    Aula de JS

---
+++ JavaScript/aula_1-Tipos-de-Variaveis/js/app.js
@@ -0,0 +1,44 @@
+//Comentário de 1 linha
+
+/*
+Comentário de várias linhas
+*/
+
+/*
+Alert - Interação de pop-up com o usuário 
+alert("Bem vindo ao meu Site");
+*/
+
+/*
+Utilizando as ferramentas de desenvolvedor
+console.log("Seja bem vindo");
+*/
+
+
+// Tipos de Variáveis
+
+let num1 = 10; // tipo Number
+let num2 = 7.5; // tipo Number
+let nome = "Iago"; // tipo String
+let sobrenome = "Oliveira"; // Tipo String
+let maiorIdade = true; // tipo Boolean
+let resposta; // tipo Undefined (Valor não definido)
+let saldo = null; // tipo nulo (Valor existemais está declarado como vazio)
+
+console.log(num1);
+console.log(num2);
+console.log(nome);
+console.log(sobrenome);
+console.log(maiorIdade);
+console.log(resposta);
+console.log(saldo);
+
+// Descobrindo o tipo de variável
+
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof nome);
+console.log(typeof sobrenome);
+console.log(typeof maiorIdade);
+console.log(typeof resposta);
+console.log(typeof saldo);
